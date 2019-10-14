1a15f4b0dfb1d24dd7ee65f8df80d247c6ad0922
bhargav jani <bhargav.jani@specindia.com>
2019-10-14 18:36:42 +0530

    Initial commit

---
+++ app/src/main/java/com/spec/imagepic_storesdcard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.spec.imagepic_storesdcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
