ffb2754442e7c2d30d09405153f6a9dd14d105f7
bhargav.jani <bhargav.jani@spec-india.com>
2019-10-14 18:39:46 +0530

    first commit

--- app/src/main/java/com/spec/imagepic_storesdcard/MainActivity.kt
+++ app/src/main/java/com/spec/imagepic_storesdcard/MainActivity.kt
@@ -2,9 +2,11 @@ package com.spec.imagepic_storesdcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    var btn: Button? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
